e4ec6b8e868d3ccda1f02f1491c9f7c914fb1018
BarryDong <286084845@qq.com>
2023-01-17 21:36:23 +0800

    编写路由链接时，直接使用 Link 标签会报错

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 function App() {
   return (
     <div>
@@ -9,8 +11,16 @@ function App() {
       <div className="row">
         <div className="col-xs-2 col-xs-offset-2">
           <div className="list-group">
-            <a className="list-group-item" href="./about.html">About</a>
-            <a className="list-group-item active" href="./home.html">Home</a>
+
+            {/*原生 HTML 中，靠 a 标签跳转到不同的页面*/}
+            {/*<a className="list-group-item" href="./about.html">About</a>*/}
+            {/*<a className="list-group-item active" href="./home.html">Home</a>*/}
+
+            {/*React 中，靠路由链接实现切换组件*/}
+            {/*编写路由链接*/}
+            <Link className="list-group-item" to="/about">About</Link>
+            <Link className="list-group-item" to="/home">Home</Link>
+
           </div>
         </div>
         <div className="col-xs-6">
