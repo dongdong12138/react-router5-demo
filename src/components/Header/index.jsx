aee614d3ec5ded722f6b79c711cd460c7988c768
BarryDong <286084845@qq.com>
2023-01-19 17:15:19 +0800

    比较一般组件与路由组件的 props

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class Header extends Component {
   render() {
+    // console.log('Header this.props:', this.props)
     return (
       <div className="page-header">
         <h2>React Router Demo</h2>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render() {
+    // console.log('Home this.props:', this.props)
     return (
       <h3>我是Home的内容</h3>
     )
